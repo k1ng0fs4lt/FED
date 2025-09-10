2591786fa92fa74fe7e7b85e4dcc0774f0354497
k1ng0fs4lt <kevin.yip@hva.nl>
2025-09-10 23:23:13 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
-// JavaScript Document
-console.log("hi");
+// JavaScript for opening and closing the menu
+
+const openBtn = document.querySelector(".open-menu");  // Open button selector
+const closeBtn = document.querySelector(".close-menu");  // Close button selector
+const navBar = document.querySelector("nav");  // Target the nav element
+
+openBtn.onclick = openMenu;
+closeBtn.onclick = closeMenu;
+
+function openMenu() {
+  navBar.classList.add("toonMenu");  // Show the menu
+}
+
+function closeMenu() {
+  navBar.classList.remove("toonMenu");  // Hide the menu
+}
